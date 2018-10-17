de0c2e962771366417e826bc48e84c26fe7bd5c2
Joshua Granick <jgranick@users.noreply.github.com>
2018-10-17 15:10:48 -0700

    Add initial patch JSON script

--- script.hx
+++ script.hx
@@ -3,7 +3,9 @@ package;
 
 import haxe.remoting.HttpConnection;
 import haxe.remoting.Proxy;
+import haxe.zip.*;
 import haxe.Http;
+import haxe.Json;
 import haxe.Serializer;
 import haxe.Timer;
 import haxe.Unserializer;
@@ -114,7 +116,42 @@ class Script extends hxp.Script {
 					}
 					
 				}
+			
+			case "patch":
 				
+				Log.info ("Patch:");
+				var projects = [];
+				
+				if (commandArgs.length > 0) {
+					
+					projects.push (commandArgs[0]);
+					
+				} else {
+					
+					projects = getProjectList (true);
+					
+				}
+				
+				for (project in projects) {
+					
+					var versions = getProjectVersions (project, true);
+					var downloadPath = Path.combine ("out/download", project);
+					
+					for (version in versions) {
+						
+						var filePath = Path.combine (downloadPath, version + ".zip");
+						if (FileSystem.exists (filePath)) {
+							
+							var outputDirectory = "out/staging/" + project + "/" + Data.safe (version);
+							unzip (filePath, outputDirectory);
+							patch (outputDirectory);
+							
+						}
+						
+					}
+					
+				}
+			
 			default:
 			
 		}
@@ -265,6 +302,173 @@ class Script extends hxp.Script {
 		}
 		
 		return versions;
+		
+	}
+	
+	
+	private static function findProjectFolder (basePath:String, path:String):String {
+		
+		var directory = Path.combine (basePath, path);
+		
+		for (file in FileSystem.readDirectory (directory)) {
+			
+			if (file == "haxelib.json") return directory;
+			
+			var fullPath = Path.combine (directory, file);
+			var childPath = Path.combine (path, file);
+			
+			if (FileSystem.isDirectory (fullPath)) {
+				
+				var path = findProjectFolder (basePath, childPath);
+				if (path != null) return path;
+				
+			}
+			
+		}
+		
+		return null;
+		
+	}
+	
+	
+	private function patch (stagingDirectory:String):Void {
+		
+		var root = findProjectFolder (stagingDirectory, "");
+		
+		if (root != null) {
+			
+			var packageJSON = Path.combine (root, "package.json");
+			
+			if (!FileSystem.exists (packageJSON)) {
+				
+				Log.info ("Patching " + stagingDirectory);
+				var info = Data.readData (File.getContent (Path.combine (root, "haxelib.json")), false);
+				
+				var json:Dynamic = {
+					
+					name: "@haxelib/" + info.name,
+					license: Std.string (info.license),
+					version: Std.string (info.version),
+					contributors: info.contributors
+					
+				}
+				
+				if (Reflect.hasField (info, "url")) {
+					json.homepage = info.url;
+				}
+				
+				if (Reflect.hasField (info, "description")) {
+					json.description = info.description;
+				}
+				
+				if (Reflect.hasField (info, "tags")) {
+					json.keywords = info.tags;
+				}
+				
+				if (Reflect.field (info, "dependencies")) {
+					var dependencies:Dynamic = {};
+					for (dependency in info.dependencies) {
+						if (dependency.type == Haxelib) {
+							Reflect.setField (dependencies, dependency.name, Std.string (dependency.version));
+						} else if (dependency.url != null) {
+							Reflect.setField (dependencies, dependency.name, dependency.url);
+						}
+					}
+					json.devDendencies = dependencies;
+				}
+				
+				File.saveContent (packageJSON, Json.stringify (json, null, "  "));
+				
+			}
+			
+		}
+		
+	}
+	
+	
+	private function unzip (filepath:String, output:String):Void {
+		
+		// Not sure haxe.zip.Reader works properly in eval target
+		
+		System.mkdir (output);
+		Log.info ("Extracting " + filepath);
+		if (FileSystem.exists (output)) System.removeDirectory (output);
+		System.runProcess ("", "unzip", [ filepath, "-d", output ]);
+		
+		// // read zip content
+		// trace (filepath);
+		// filepath = Path.tryFullPath (filepath);
+		// trace (filepath);
+		// var f = File.read(filepath,true);
+		// var zip = try {
+		// 	Reader.readZip(f);
+		// } catch (e:Dynamic) {
+		// 	f.close();
+		// 	// file is corrupted, remove it
+		// 	// if (!nodelete)
+		// 		// FileSystem.deleteFile(filepath);
+		// 	throw e;
+		// }
+		// f.close();
+		// var infos = Data.readInfos(zip,false);
+		// Log.info('Staging ${infos.name}...');
+		// // create directories
+		// var target = output;
+		// target += "/";
+
+		// // locate haxelib.json base path
+		// var basepath = Data.locateBasePath(zip);
+
+		// // unzip content
+		// var entries = [for (entry in zip) if (StringTools.startsWith (entry.fileName, basepath)) entry];
+		// var total = entries.length;
+		// for (i in 0...total) {
+		// 	var zipfile = entries[i];
+		// 	var n = zipfile.fileName;
+		// 	// remove basepath
+		// 	n = n.substr(basepath.length,n.length-basepath.length);
+		// 	if( n.charAt(0) == "/" || n.charAt(0) == "\\" || n.split("..").length > 1 )
+		// 		throw "Invalid filename : "+n;
+
+		// 	// if (settings.debug) {
+		// 	// 	var percent = Std.int((i / total) * 100);
+		// 	// 	Sys.print('${i + 1}/$total ($percent%)\r');
+		// 	// }
+
+		// 	var dirs = ~/[\/\\]/g.split(n);
+		// 	var path = "";
+		// 	var file = dirs.pop();
+		// 	for( d in dirs ) {
+		// 		path += d;
+		// 		// safeDir(target+path);
+		// 		path += "/";
+		// 	}
+		// 	if( file == "" ) {
+		// 		// if( path != "" && settings.debug ) print("  Created "+path);
+		// 		continue; // was just a directory
+		// 	}
+		// 	path += file;
+		// 	// if (settings.debug)
+		// 	// 	print("  Install "+path);
+		// 	var data = Reader.unzip(zipfile);
+		// 	File.saveBytes(target+path,data);
+		// }
+
+		// // set current version
+		// // if( setcurrent || !FileSystem.exists(pdir+".current") ) {
+		// // 	File.saveContent(pdir + ".current", infos.version);
+		// // 	print("  Current version is now "+infos.version);
+		// // }
+
+		// // end
+		// // if( !nodelete )
+		// // 	FileSystem.deleteFile(filepath);
+		// // print("Done");
+
+		// // process dependencies
+		// // doInstallDependencies(rep, infos.dependencies);
+
+		// // return infos;
 		
 	}
 	
